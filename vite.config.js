af9cf87f8b4cf70ce7fb3c7d17ba7a447f2e1256
Pavan <pavanchytanya@gmail.com>
2024-11-02 21:41:03 +0530

    base name change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Animaitions",
+  base: "/Animaitons",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
